561ab66c3c8be0130125a4baae68a4fb87f16652
jonathanrsr <jonathan.ryser@epfl.ch>
2024-03-08 22:37:25 +0100

    Changes to be committed: 	new file:   Projects/1. Enzyme and microbial kinetics/Exercise 2 - old.py 	modified:   Projects/1. Enzyme and microbial kinetics/Exercise 2.py
    First draft of f cell and lactose concentration graph finished. Need to
    order the code and ask the other group members about it.

--- Projects/1. Enzyme and microbial kinetics/Exercise 2.py
+++ Projects/1. Enzyme and microbial kinetics/Exercise 2.py
@@ -1,47 +1,29 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
-from numpy.polynomial import Polynomial
 
-time_cell = np.array([0.0, 0.54, 0.9, 1.23, 1.58, 1.95, 2.33, 2.7])
-cell_concentration = np.array([15.5, 23.0, 30.0, 38.8, 48.5, 58.3, 61.3, 62.5])
-time_lactose = np.array([0.0, 0.54, 0.54, 0.9, 0.9, 1.23, 1.23, 1.58, 1.58, 1.95, 1.95, 2.33, 2.33, 2.7])
-lactose_concentration = np.array([137.0, 137.0, 114.0, 114.0, 90.0, 90.0, 43.0, 43.0, 29.0, 29.0, 9.0, 9.0, 2.0, 2.0])
+# Empirical data
+sampling_time = np.array([0, 0.54, 0.9, 1.23, 1.58, 1.95, 2.33, 2.7]) # h
+cell_concentration = np.array([15.5, 23.0, 30.0, 38.8, 48.5, 58.3, 61.3, 62.5]) # g/L
+lactose_concentration = np.array([137.0, 114.0, 90.0, 43.0, 29.0, 9.0, 2.0]) # g/L
 
-plt.plot(time_cell, cell_concentration, label = 'Cell concentration (g/L)')
-#plt.plot(time_lactose, lactose_concentration, label = 'Lactose concentration (g/L)')
-plt.legend()
+# Plot cell and lactose concentration as a function of time
+fig, ax1 = plt.subplots()
+ax1.stairs(lactose_concentration, sampling_time, baseline = None, color = 'r', linestyle = '--', linewidth = 1.0, label = 'Lactose concentration')
+#ax1.plot(sampling_time[:-1] + np.diff(sampling_time)/2, lactose_concentration, color = 'r', marker = 'o', linestyle = 'None')
+ax1.set_ylabel('Lactose concentration, $\mathregular{g.L^{-1}}$')
+ax1.set_yticks(np.linspace(0, 160, 9))
+ax1.legend(loc = 'upper left')
+ax1.grid(linewidth = 0.25)
+
+ax2 = ax1.twinx()
+ax2.plot(sampling_time, cell_concentration, color = 'b', marker = 'o', markersize = '4', linestyle = '--', linewidth = 1.0, label = 'Cell concentration')
+ax2.set_xlabel('Time, h')
+ax2.set_ylabel('Cell concentration, $\mathregular{g.L^{-1}}$')
+ax2.grid(linewidth = 0.25)
+ax2.set_yticks(np.linspace(0, 80, 9))
+ax2.legend(loc = 'upper right')
+
+plt.title('Cell and lactose concentration versus time')
+
 plt.show()
-
-plt.plot(time_cell, np.log(cell_concentration))
-plt.show()
-
-mu = np.zeros(7)
-for x in range(len(mu)):
-    mu[x] = 1/cell_concentration[x]*(cell_concentration[x + 1] - cell_concentration[x])/(time_cell[x + 1] - time_cell[x])
-
-
-lactose_plot = np.array([137, 114, 90, 43, 29, 9, 2])
-plt.plot(1/lactose_plot, 1/mu)
-plt.show()
-
-mu_exp = mu[0:5]
-lactose_plot = np.array([137, 114, 90, 43, 29])
-
-slope = Polynomial.fit(1/lactose_plot, 1/mu_exp, 1).convert().coef
-print(slope)
-
-y = 1/lactose_plot*slope[1] + slope[0]
-
-#Faire pente jusqua abscisse
-
-plt.plot(1/lactose_plot, 1/mu_exp, 1/lactose_plot, y)
-plt.ylim([0, 2])
-plt.xlim([0, 0.035])
-plt.show()
-
-mu_max = 1/slope[0]
-Ks = slope[1]*mu_max
-
-print(mu_max, Ks)
-
-dt = np.log(2)/mu
